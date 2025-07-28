ee296251280137fb0d8dfff3deaeacbd62f1f0ff
its-kronos <anseld41@gmail.com>
2025-07-27 22:35:17 -0400

    USB UART bridge now works due to another repo with a C implementation (with 115200 baud!)

--- Code/boot.py
+++
@@ -1,5 +0,0 @@
-# install circuit python!!!
-
-import usb_cdc
-
-usb_cdc.enable(console = True, data = True)
---
+++ Production/Test-Code/WROOM.py
@@ -0,0 +1,3 @@
+# Assumes that micropython is installed on the WROOM module
+# To flash after micropython is installed, please use the USB UART bridge given in the firmware folder and use any tool to flash, running at 115200 baud
+
